98e20429f7bc6f66b487f3bbf248a03296292490
Tabinda Hashmat <159946123+TabindaHashmat@users.noreply.github.com>
2024-03-31 19:25:36 +0500

    Add files via upload
    here is todos List coding.

---
+++ main.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const main = async () => {
+    let todos = [];
+    let condition = "true";
+    while (condition) {
+        let addTask = await inquirer.prompt([
+            {
+                name: "todo",
+                type: "input",
+                message: "what do you want to add to your Todos?"
+            },
+            {
+                name: 'addMore',
+                type: 'confirm',
+                message: 'Do you want to add more?',
+                default: "false",
+            }
+        ]);
+        todos.push(addTask.todo);
+        condition = addTask.addMore;
+        console.log(todos);
+    }
+};
+main();
